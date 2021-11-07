f19ef34a6c8306ee3f07be86172aa3dd0e6a4c58
jigsalt <joshuanuestro@gmail.com>
2021-11-08 01:59:00 +0800

    backend

--- server.js
+++ server.js
@@ -26,18 +26,7 @@ app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
 
-// app.get('/api/products/:id', (req, res) => {
-//   const product = data.products.find((x) => x._id === req.params.id);
-//   if (product) {
-//      res.send(product);
-//   } else {
-//      res.status(404).send({ message: 'Product Not Found' });
-//   }
-// });
 
-// app.get('/api/products',(req,res)=>{
-//     res.send(data.products);
-// }) //commented out because static
 
 app.get('/',(req,res)=>{
     res.send('Server is ready');
